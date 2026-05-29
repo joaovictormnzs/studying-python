56403e6c3c282b6b28df1ffc8743410b4b3f1e69
joaovictormnzsl <jvictormenezesl@gmail.com>
2026-05-29 17:46:48 -0300

    Aprendendo sobre propriedades.

--- Python/programacao-orientada-objeto/joao-store/modelos/produto.py
+++ Python/programacao-orientada-objeto/joao-store/modelos/produto.py
@@ -2,19 +2,30 @@ class Produto:
     produtos = []
 
     def __init__(self, nome, categoria):
-        self.nome = nome 
-        self.categoria = categoria 
-        self.ativo = False
+        self._nome = nome.title() 
+        self._categoria = categoria.upper()
+        self._ativo = False
         Produto.produtos.append(self)
 
     def __str__(self):
-        return f'{self.nome} | {self.categoria}' 
+        return f'{self._nome} | {self._categoria}' 
     
-    def listar_produtos():
-        for produto in Produto.produtos:
-            print(f'Produto: {produto.nome} | Categoria: {produto.categoria} | Ativo: {produto.ativo}')
+    @classmethod
+    def listar_produtos(cls):
+        print(f'{'Nome do Produto'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
+        for produto in cls.produtos:
+            print(f'{produto._nome.ljust(25)} | {produto._categoria.ljust(25)} | {produto.ativo}')
+            
+    @property
+    def ativo(self):
+        return 'verdadeiro' if self._ativo else 'falso'
+    
+    def alternar_estado(self):
+        self._ativo = not self._ativo
+    
 
 computador = Produto('Computador', 'Eletronico')
+computador.alternar_estado()
 celular = Produto('Celular', 'Eletronico')
 
 Produto.listar_produtos()
